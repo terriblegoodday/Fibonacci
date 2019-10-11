f0c71e2a0850fdee4d3482c706eaef86585ed6c7
Eduard <terriblegoodday@icloud.com>
2019-10-12 09:25:25 +1000

    Initial Commit

---
+++ Fibonacci/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fibonacci
+//
+//  Created by Eduard Dzhumagaliev on 10/12/19.
+//  Copyright © 2019 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
